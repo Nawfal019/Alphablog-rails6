c553f298147375f4552f56af0c75d1fffeb3c9f8
Nawfal Imran <nawfal@deltalabs.io>
2023-04-03 13:27:15 +0500

    add one-to-many association between articles and users from backend

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_30_163426) do
+ActiveRecord::Schema.define(version: 2023_04_03_075552) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
