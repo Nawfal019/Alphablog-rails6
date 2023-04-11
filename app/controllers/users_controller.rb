a9392c4393913ea012ddf701702bfd016119f403
Nawfal Imran <nawfal@deltalabs.io>
2023-04-11 19:19:19 +0500

    navigation checks for UI in user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:show, :edit, :update]
   def new
     @user = User.new    
   end
 
   def show
-    @user = User.find(params[:id])
     @articles= @user.articles.paginate(page: params[:page], per_page: 4)
   end
 
@@ -16,6 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = user.id
       flash[:notice] = "Welcome to Alpha blog, #{@user.username} has successfully signed up."
       redirect_to articles_path
     else
@@ -24,11 +24,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice]="The update was successful"
       redirect_to @user
@@ -40,6 +38,10 @@ class UsersController < ApplicationController
 private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
